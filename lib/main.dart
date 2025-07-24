9df752ead9186889500fa2992bf92bae75f0abc1
stanlapru <stanlap@vk.com>
2025-07-24 13:08:44 +0300

    Added desktop layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/pages/first_page.dart';
 import 'package:flutter_application_2/pages/second_page.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Занятие 3',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 183, 177, 58),
+          seedColor: const Color.fromARGB(255, 9, 255, 0),
         ),
       ),
       home: const MainPage(),
@@ -40,32 +41,78 @@ class _MainPageState extends State<MainPage> {
     const ThirdPage(),
   ];
 
+  bool get isDesktop =>
+      kIsWeb ||
+      defaultTargetPlatform == TargetPlatform.linux ||
+      defaultTargetPlatform == TargetPlatform.windows ||
+      defaultTargetPlatform == TargetPlatform.macOS;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_current_screen],
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.looks_one_rounded),
-            label: "Первый",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.looks_two_rounded),
-            label: "Второй",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.three_g_mobiledata),
-            label: "Третий",
-          ),
-        ],
-        currentIndex: _current_screen,
-        onTap: (value) {
-          setState(() {
-            _current_screen = value;
-          });
-        },
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (isDesktop && constraints.maxWidth > 600) {
+          // Desktop
+          return Scaffold(
+            body: Row(
+              children: [
+                NavigationRail(
+                  destinations: const [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.looks_one_rounded),
+                      label: Text("Первый"),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.looks_two_rounded),
+                      label: Text("Второй"),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.three_g_mobiledata),
+                      label: Text("Третий"),
+                    ),
+                  ],
+                  labelType: NavigationRailLabelType.all,
+                  selectedIndex: _current_screen,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      _current_screen = value;
+                    });
+                  },
+                ),
+                const VerticalDivider(thickness: 1, width: 1,),
+                Expanded(child: _pages[_current_screen],)
+              ],
+            ),
+          );
+        } else {
+          // Mobile
+          return Scaffold(
+            body: _pages[_current_screen],
+            bottomNavigationBar: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.looks_one_rounded),
+                  label: "Первый",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.looks_two_rounded),
+                  label: "Второй",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.three_g_mobiledata),
+                  label: "Третий",
+                ),
+              ],
+              currentIndex: _current_screen,
+              onTap: (value) {
+                setState(() {
+                  _current_screen = value;
+                });
+              },
+            ),
+          );
+        }
+      },
     );
   }
 }
--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -7,12 +7,46 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Первая страница')),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Icon(Icons.fitness_center_outlined),
-            SizedBox(height: 8),
-            Text('Это первая страница!'),
+            FeedCard(title: '123', text: 'abcd', color: Colors.amber),
+            FeedCard(title: '12j3', text: 'abcdehthdthhh', color: Colors.greenAccent),
+            FeedCard(title: '1jghjhg23', text: 'abcdjghjghjgj', color: const Color.fromARGB(255, 224, 105, 240)),
+            FeedCard(title: 'jjgjh', text: 'abchgjhjgjd', color: Colors.greenAccent),
+            FeedCard(title: '12jghj3', text: 'abghjghjghjghcd', color: const Color.fromARGB(255, 118, 105, 240)),
+            FeedCard(title: '12ghjjhg3', text: 'abcjhjghjghjgjgd', color: Colors.greenAccent),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FeedCard extends StatelessWidget {
+  final Color color;
+  final String title;
+  final String text;
+  const FeedCard({super.key, required this.color,  required this.title, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color.fromARGB(255, 206, 206, 206),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+            Container(
+              color: color,
+              height: 200,
+            ),
+            Text(title, style: TextStyle(fontSize: 28)),
+            Text(
+              text,
+            ),
           ],
         ),
       ),
